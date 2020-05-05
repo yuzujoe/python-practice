53edcd4d00e604c8777508542c29585b14f5a9a8
yuzujoe <yuzun225@gmail.com>
2020-05-05 11:21:59 +0900

    string slice

--- basic-syntax/string.py
+++ basic-syntax/string.py
@@ -20,3 +20,22 @@ print('py' + 'thon')
 
 print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa'
       'bbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+
+##########
+# slice
+##########
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print("##############")
+print(word[0:2])
+print(word[:2])
+print("##############")
+print(word[2:])
+
+word = 'j' + word[1:]
+print(word)
