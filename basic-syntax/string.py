46707dc73d6218fac8d07f12eda93179e409f903
yuzujoe <yuzun225@gmail.com>
2020-05-05 11:13:05 +0900

    string

---
+++ basic-syntax/string.py
@@ -0,0 +1,22 @@
+print("hello")
+print('hello')
+print('I don\'t know')
+print("I don't know")
+
+print('Hello \nHow are you')
+print(r'C:\name\name')
+
+print("##############")
+print("""\
+  line1
+  line2
+  line3\
+""")
+print("##############")
+
+print('Hi.' * 3 + 'Mike.')
+
+print('py' + 'thon')
+
+print('aaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      'bbbbbbbbbbbbbbbbbbbbbbbbbbbb')
