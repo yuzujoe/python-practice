4aa7add24aa35a6e5685cd788afceb38703639d8
yuzujoe <yuzun225@gmail.com>
2020-05-05 11:33:51 +0900

    string method

--- basic-syntax/string.py
+++ basic-syntax/string.py
@@ -39,3 +39,20 @@ print(word[2:])
 
 word = 'j' + word[1:]
 print(word)
+
+s = 'My name is Mike. Hi Mike.'
+print(s)
+is_start = s.startswith('My')
+print(is_start)
+is_start = s.startswith('X')
+print(is_start)
+
+print("##############")
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
