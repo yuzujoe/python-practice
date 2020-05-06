ac6b9d177c523a37022761215f9827e861138340
yuzujoe <yuzun225@gmail.com>
2020-05-06 16:14:46 +0900

    tapple

---
+++ data-structure/taplle.py
@@ -0,0 +1,36 @@
+t = (1, 2, 3, 4, 1, 2)
+print(type(t))
+
+print(t[2:5])
+print(t.index(1))
+
+print(t.index(1, 1))
+
+print(t.count(1))
+
+t = 1, 2, 3
+print(type(t))
+
+new_taple = (1, 2, 3) + (4, 5, 6)
+print(new_taple)
+
+num_taple = (10, 20)
+print(num_taple)
+
+x, y = num_taple
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+a = 100
+b = 200
+a, b = b, a
+print(a, b)
+
+chose_from_two = ('A', 'B', 'C')
+
+answer = []
+
+print(chose_from_two)
+print(answer)
