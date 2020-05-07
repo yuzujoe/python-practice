8c937d01665dc93953a746022989b481b76d7179
yuzujoe <yuzun225@gmail.com>
2020-05-07 09:16:15 +0900

    if

---
+++ data-structure/if.py
@@ -0,0 +1,30 @@
+x = [1, 2, 3]
+y = 1
+
+# if x in y:
+#     print('in')
+
+# if 100 not in y:
+#     print('not in')
+
+a = 1
+b = 2
+
+if not a == b:
+    print('not equal')
+
+if a < b:
+    print('not equal')
+
+# False 0, 0.0, "", []
+is_ok = 1
+
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
+
+is_empty = None
+# Objectをみている
+if is_empty is None:
+    print('None!')
