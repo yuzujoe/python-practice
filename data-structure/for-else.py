78ce76eee01f8c2eca14bdbccad501c688507b98
yuzujoe <yuzun225@gmail.com>
2020-09-06 14:35:47 +0900

    for

---
+++ data-structure/for-else.py
@@ -0,0 +1,7 @@
+for fruits in ["apple", "banana", "orange"]:
+    if fruits == "banana":
+        print("stop eating")
+        break
+    print(fruits)
+else:
+    print("I eats")
---
+++ data-structure/for.py
@@ -0,0 +1,9 @@
+some_list = [1, 2, 3, 4, 5]
+
+for i in some_list:
+    print(i)
+
+for word in ["my", "name", "is", "mike"]:
+    if word == "name":
+        continue
+    print(word)
