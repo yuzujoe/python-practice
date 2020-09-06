20f29027844e3331866d2e5a1fec18756e006b5a
yuzujoe <yuzun225@gmail.com>
2020-09-06 14:43:01 +0900

    zip

---
+++ data-structure/zip.py
@@ -0,0 +1,6 @@
+days = ['Mon', 'Tue', 'Wed']
+fruits = ["apple", "banana", "orange"]
+drinks = ['coffee', 'tea', 'beer']
+
+for days, fruits, drinks in zip(days, fruits, drinks):
+    print(days, fruits, drinks)
